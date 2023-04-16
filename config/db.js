6540323d54f3fdbd5db7daf52e1ed49c87cafcbe
Nikhil Web <contactnikhil360@gmail.com>
2023-04-16 21:13:44 +0530

    all is working in dev

---
+++ config/db.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+export const connectDB = () => {
+  mongoose
+    .connect(process.env.MONGO_URI, {
+      dbName: "ToDo2",
+    })
+    .then(() => console.log("Database Connected"))
+    .catch((e) => console.log(e));
+};
---
+++ models/task.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema({
+  title: {
+    type: String,
+  },
+  description: {
+    type: String,
+    require: true,
+  },
+  isCompleted: {
+    type: Boolean,
+    default: false,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export const Task = mongoose.model("Task", schema);
